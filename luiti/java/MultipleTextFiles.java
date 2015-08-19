552b2e8758dae13f070e931adf82f6146c05755c
David Chen <mvjome@gmail.com>
2015-08-19 16:01:28 +0800

    Translate MultipleTextFiles document

--- luiti/java/MultipleTextFiles.java
+++ luiti/java/MultipleTextFiles.java
@@ -6,17 +6,17 @@ import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 
 public class MultipleTextFiles extends MultipleTextOutputFormat<Text, Text> {
     /**
-     * 目前在 luiti 里采用的 reducer 函数里面是用
+     * Currently, the `reducer` function in luiti use below data format.
      *     yield "", "{"json key": "json value"}"
-     * 格式的。所以现在分多个目录的话，就用没用上的 key 就好了。
+     *  If need multiple file output, then we use the unused yield key.
      *
-     * 代码参考: http://blog.csdn.net/lmc_wy/article/details/7532213
+     * Ref code: http://blog.csdn.net/lmc_wy/article/details/7532213
      */
 
     protected String generateFileNameForKeyValue(Text key, Text value, String name)
     {
-        String outputName = key.toString();      // 取得当前文件名
-        key.set("");                             // 输出里只需要 value(json 格式) 就可以了。
+        String outputName = key.toString();      // Get the current filename
+        key.set("");                             // We just need the value, so remove the unneeded key.
         return new Path(outputName, name).toString();   // 参考 https://github.com/klbostee/feathers
     }
 
